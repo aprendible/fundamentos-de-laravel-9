f2ba8e38b9070b34d2eaee60110edc384a10b6f5
Jorge García <jorge@aprendible.com>
2022-03-14 18:16:48 -0500

    05 - Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// personal-site.com => welcome
+// personal-site.com/contacto => contact
+// personal-site.com/blog => blog
+// personal-site.com/about => about
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/contacto', 'contact');
+Route::view('/blog', 'blog');
+Route::view('/about', 'about');
