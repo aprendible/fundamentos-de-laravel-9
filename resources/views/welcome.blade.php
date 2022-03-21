37b2bd05674db43891aae7d0ae364c27a194f8c3
Jorge García <jorge@aprendible.com>
2022-03-21 17:21:06 -0500

    06 - Rutas con nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,12 @@
     <title>Inicio</title>
 </head>
 <body>
+<ul>
+    <li><a href="<?= route('home') ?>">Home</a></li>
+    <li><a href="<?= route('blog') ?>">Blog</a></li>
+    <li><a href="<?= route('about') ?>">About</a></li>
+    <li><a href="<?= route('contact') ?>">Contact</a></li>
+</ul>
 <h1>Inicio</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 // personal-site.com/blog => blog
 // personal-site.com/about => about
 
-Route::view('/', 'welcome');
-Route::view('/contacto', 'contact');
-Route::view('/blog', 'blog');
-Route::view('/about', 'about');
+Route::view('/', 'welcome')->name('home');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/blog', 'blog')->name('blog');
+Route::view('/about', 'about')->name('about');
