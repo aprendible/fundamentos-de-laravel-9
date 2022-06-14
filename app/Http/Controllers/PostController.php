7950e0303f28f94cf4a41cf520109ae8edab60e1
Aprendible <jorge@aprendible.com>
2022-06-13 20:30:30 -0500

    17 - Mensajes de sesión

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,6 +31,8 @@ class PostController extends Controller
         $post->body = $request->input('body');
         $post->save();
 
+        session()->flash('status', 'Post created!');
+
         return to_route('posts.index');
     }
 }
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,6 +11,10 @@
 <body>
 <x-layouts.navigation />
 
+@if(session('status'))
+    <div>{{ session('status') }}</div>
+@endif
+
 {{ $slot }}
 
 </body>
