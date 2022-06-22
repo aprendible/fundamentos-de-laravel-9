0d60508bfb61f23163a4b630372051e149f0f04b
Aprendible <jorge@aprendible.com>
2022-06-21 19:03:19 -0500

    18 - Validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,6 +26,11 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => ['required', 'min:4'],
+            'body' => ['required'],
+        ]);
+
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,11 +8,19 @@
         @csrf
         <label>
             Title <br>
-            <input name="title" type="text">
+            <input name="title" type="text" value="{{ old('title') }}">
+            @error('title')
+                <br>
+                <small style="color: red">{{ $message }}</small>
+            @enderror
         </label><br>
         <label>
             Body <br>
-            <textarea name="body"></textarea>
+            <textarea name="body">{{old('body')}}</textarea>
+            @error('body')
+                <br>
+                <small style="color: red">{{ $message }}</small>
+            @enderror
         </label><br>
         <button type="submit">Enviar</button>
         <br>
