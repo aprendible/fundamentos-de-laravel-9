918d893457e803d1ef500b01dba104c44a7fe4ce
Aprendible <jorge@aprendible.com>
2022-08-20 13:14:27 -0500

    27 - Instalación de Tailwind CSS

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aprendible - {{ $title ?? '' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default meta description' }}" />
+    @vite(['resources/js/app.js'])
 </head>
 <body>
 <x-layouts.navigation />
